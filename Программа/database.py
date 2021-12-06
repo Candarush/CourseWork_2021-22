872863e1b3071d80c6cc45464ac30197a7149583
Akimov Vladislav <candarush@gmail.com>
2021-12-06 03:21:41 +0300

    Initial commit

---
+++ Программа/database.py
@@ -0,0 +1,112 @@
+import sqlite3
+
+
+class my_db():
+
+    def __init__(self, bd):
+        self.bd = bd
+        self.curr = bd.cursor()
+        self.curr.execute("""
+                create table if not exists Authorization(
+                    authorization_id integer primary key autoincrement,
+                    login char(16),
+                    password char(16)
+                )""")
+        self.curr.execute("""
+                create table if not exists Products(
+                    product_id integer primary key autoincrement,
+                    name char(16),
+                    price integer,
+                    article char(20)
+                )""")
+        self.curr.execute("""
+                create table if not exists Buyers(
+                    buyer_id integer primary key autoincrement,
+                    name char(16),
+                    surname char(16),
+                    discount double 
+                )""")
+        self.curr.execute("""
+                create table if not exists Store(
+                    store_id integer primary key autoincrement,
+                    name char(16),
+                    address char(64) 
+                )""")
+        self.curr.execute("""
+                create table if not exists History(
+                    history_id integer primary key autoincrement,
+                    product_id integer,
+                    buyer_id integer,
+                    store_id integer,
+                    buy_date date,
+                    foreign key (product_id) references products (product_id),
+                    foreign key (buyer_id) references buyers (buyer_id),
+                    foreign key (store_id) references store (store_id)
+                    )""")
+        self.bd.commit()
+
+    def select_all(self):
+        """
+        Возвращает список кортежей, в каждом кортеже указаны все данные.
+        """
+        self.curr.execute(""" 
+        SELECT Products.name,Products.price,article,Buyers.name,Buyers.surname,
+        Buyers.discount,Store.name,Store.address,buy_date FROM History
+        join Products on History.product_id=Products.product_id
+        join Buyers on History.buyer_id=Buyers.buyer_id
+        join Store on History.store_id=Store.store_id
+        
+        """)
+        return self.curr.fetchall()
+
+    def get_by_name(self, name, surname):
+        """
+        Принимает две строки - имя и фамилия, по которым осуществляется фильтрация.
+
+        Возвращает список из кортежей, где каждый кортеж -  Имя товара, цена, арткул, имя магазина, адрес магазина,
+        дата покупки.
+        """
+        self.curr.execute(f"""
+        SELECT Products.name,Products.price,article,Store.name,Store.address,buy_date FROM History
+        join Products on History.product_id=Products.product_id
+        join Buyers on History.buyer_id=Buyers.buyer_id
+        join Store on History.store_id=Store.store_id
+        where Buyers.name='{name}' and Buyers.surname='{surname}'""")
+        return self.curr.fetchall()
+
+    def get_by_article(self, article):
+        """
+        Принимает строку - артикул, по которому осуществляется фильтрация
+
+        Возвращает список из кортежей, где каждый кортеж - имя товара,цену товара,имя покупателя, фамилиюю покупателя,
+        его скидка, имя магазина, адрес магазина,
+        дату покупки.
+        """
+        self.curr.execute(f"""SELECT Products.name,Products.price,Buyers.name,Buyers.surname,Buyers.discount,Store.name,
+        Store.address,buy_date FROM History
+        join Products on History.product_id=Products.product_id
+        join Buyers on History.buyer_id=Buyers.buyer_id
+        join Store on History.store_id=Store.store_id
+        where Products.article='{article}'""")
+        return self.curr.fetchall()
+
+    def check_user_in_authorization(self, login, password):
+        """
+        Принимает строки - логин и пароль
+        Возвращает 1, если пользователь есть в таблице авторизации
+        Возвращает 0, если пользователя нет в таблице авторизации
+        """
+        self.curr.execute(f"""select count(login) from Authorization
+                                where login = '{login}' and password='{password}'""")
+        user = self.curr.fetchall()[0][0]
+        if user == 1:
+            return 1
+        elif user == 0:
+            return 0
+
+    def close_con(self):
+        """Закрывает соединение с базой"""
+        self.bd.close()
+
+if __name__ == '__main__':
+    db = my_db(sqlite3.connect('db.sqlite'))
---
+++ Программа/i_mag_interface.py
@@ -0,0 +1,143 @@
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5.QtCore import QSize, Qt, pyqtSlot
+from PyQt5.QtGui import QFont, QTextLine
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QWidget
+import sys
+from database import my_db
+import sqlite3
+
+app = QApplication(sys.argv)
+
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+
+		self.db = my_db(sqlite3.connect('db.sqlite'))
+
+		self.setWindowTitle("IMag")
+		self.setFixedSize(QSize(1000, 600))
+
+		self.login_area = QWidget(self)
+		self.setCentralWidget(self.login_area)
+
+		self.login_text = QLabel("Войти", self.login_area)
+		self.login_text.setGeometry(440,224,120,16)
+		self.login_text.setAlignment(Qt.AlignCenter)
+
+		self.login_linedit = QLineEdit("", self.login_area)
+		self.login_linedit.setGeometry(440,240,120,30)
+
+		self.password_text = QLabel("Password", self.login_area)
+		self.password_text.setGeometry(440,274,120,16)
+		self.password_text.setAlignment(Qt.AlignCenter)
+
+		self.password_lineedit = QLineEdit("", self.login_area)
+		self.password_lineedit.setGeometry(440,290,120,30)
+		self.password_lineedit.setEchoMode(QLineEdit.Password)
+
+		self.authorize_button = QPushButton("Log in", self.login_area)
+		self.authorize_button.setGeometry(460,330,80,40)
+		self.authorize_button.clicked.connect(self.login)
+
+		self.show()
+		
+	def login(self):
+		response = self.db.check_user_in_authorization(self.login_linedit.text(), self.password_lineedit.text())
+		if response == 1:
+			self.initialize_main_window()
+		else:
+			self.password_lineedit.setText("")
+	
+	def initialize_main_window(self):
+		self.login_area.setParent(None)
+		print('Initialized.')
+
+		self.main_area = QWidget(self)
+		self.setCentralWidget(self.main_area)
+
+		self.main_table = QTableWidget(self.main_area)
+		self.main_table.setGeometry(20,80,960,500)
+		self.main_table.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+		self.main_table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn);
+		
+		self.history_button = QPushButton('История покупок', self.main_area)
+		self.history_button.setGeometry(20,20,100,25)
+		self.history_button.clicked.connect(self.show_history)
+		
+		self.bought_products_button = QPushButton('Найти покупки', self.main_area)
+		self.bought_products_button.setGeometry(130,20,230,25)
+		self.bought_products_button.clicked.connect(self.show_bought_products)
+
+		self.bought_products_name_lineedit = QLineEdit(self.main_area)
+		self.bought_products_name_lineedit.setPlaceholderText('Имя покупателя')
+		self.bought_products_name_lineedit.setGeometry(131,47,113,25)
+		self.bought_products_name_lineedit.setAlignment(Qt.AlignCenter)
+
+		self.bought_products_surname_lineedit = QLineEdit(self.main_area)
+		self.bought_products_surname_lineedit.setPlaceholderText('Фамилия покупателя')
+		self.bought_products_surname_lineedit.setGeometry(246,47,113,25)
+		self.bought_products_surname_lineedit.setAlignment(Qt.AlignCenter)
+		
+		self.product_buyers_button = QPushButton('Найти покупателей', self.main_area)
+		self.product_buyers_button.setGeometry(369,20,230,25)
+		self.product_buyers_button.clicked.connect(self.show_product_buyers)
+		
+		self.product_buyers_article_lineedit = QLineEdit(self.main_area)
+		self.product_buyers_article_lineedit.setPlaceholderText('Артикул')
+		self.product_buyers_article_lineedit.setGeometry(370,47,229,25)
+		self.product_buyers_article_lineedit.setAlignment(Qt.AlignCenter)
+
+	def show_history(self):
+		history = self.db.select_all()
+		self.show_table_data(history, ['Название товара', 'Цена', 'Артикул', 'Имя покупателя', 'Фамилия покупателя', 'Скидка', 'Название магазина', 'Адрес магазина', 'Дата покупки'])
+
+	def show_bought_products(self):
+		bought_products = self.db.get_by_name(self.bought_products_name_lineedit.text(), self.bought_products_surname_lineedit.text())
+		self.show_table_data(bought_products, ['Название товара', 'Цена', 'Арткул', 'Название магазина', 'Адрес магазина', 'Дата покупки'])
+
+	def show_product_buyers(self):
+		bought_products = self.db.get_by_article(self.product_buyers_article_lineedit.text())
+		self.show_table_data(bought_products, ['Название товара', 'Цена', 'Фамилия покупателя', 'Имя покупателя', 'Скидка', 'Название магазина', 'Адрес магазина', 'Дата покупки'])
+
+	def show_table_data(self, data, lables = ['']):
+		self.clear_main_table()
+		if (len(data)) == 0:
+			self.main_table.setColumnCount(1)
+			self.main_table.insertRow(0)
+			self.main_table.setItem(0, 0, QTableWidgetItem('Информация не найдена'))
+			self.main_table.resizeColumnsToContents()
+			self.main_table.verticalHeader().setVisible(False)
+			self.main_table.horizontalHeader().setVisible(False)
+			print('No data found to fill the table.')
+			return
+		self.main_table.verticalHeader().setVisible(True)
+		self.main_table.horizontalHeader().setVisible(True)
+		self.main_table.setColumnCount(len(data[0]))
+		for i in range(len(data)):
+			self.main_table.insertRow(i)
+			entry = data[i]
+			for j in range(len(entry)):
+				item = QTableWidgetItem(str(entry[j]))
+				item.setFlags(QtCore.Qt.ItemIsEnabled)
+				self.main_table.setItem(i, j, item)
+		self.main_table.setHorizontalHeaderLabels(lables)
+		self.main_table.resizeColumnsToContents()
+
+	def clear_main_table(self):
+		self.main_table.clearContents()
+		while self.main_table.rowCount() > 0: 
+			self.main_table.removeRow(0)
+		while self.main_table.columnCount() > 0: 
+			self.main_table.removeColumn(0)
+		
+
+
+
+
+window = MainWindow()
+window.login_linedit.setText('admin')
+window.password_lineedit.setText('1111')
+
+app.exec()
