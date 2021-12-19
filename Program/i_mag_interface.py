f4ac7a5b09520109fe745ea2536a9639c6e2b284
Akimov Vladislav <candarush@gmail.com>
2021-12-19 19:02:53 +0300

    4

--- Program/i_mag_interface.py
+++ Program/i_mag_interface.py
@@ -14,10 +14,11 @@ import sqlite3
 
 class MainWindow(QMainWindow):
 	def __init__(self):
+		"""
+			Инициализаци окна программы.
+		"""
 		super().__init__()
 		self.db = MyDb(sqlite3.connect(sys.path[1]+'/db.sqlite'))
-
-		print("Interface initialized.")
 
 		self.setWindowTitle("IMag")
 		self.setFixedSize(QSize(1000, 600))
@@ -47,6 +48,9 @@ class MainWindow(QMainWindow):
 		self.show()
 
 	def login(self):
+		"""
+			Авторизация при помощи запроса к базе данных.
+		"""
 		response = self.db.check_user_in_authorization(self.login_linedit.text(), self.password_lineedit.text())
 		print(self.login_linedit.text(), self.password_lineedit.text(), response)
 		self.db.debug_print('Authorization')
@@ -59,6 +63,9 @@ class MainWindow(QMainWindow):
 				  ", Password:" + self.password_lineedit.text())
 
 	def initialize_main_window(self):
+		"""
+			Загрузка элементов главной таблицы.
+		"""
 		self.login_area.setParent(None)
 		print('Initialized.')
 
@@ -101,17 +108,23 @@ class MainWindow(QMainWindow):
 		self.show_buyers_button.setGeometry(609, 20, 230, 25)
 		self.show_buyers_button.clicked.connect(self.show_buyers)
 		
-		self.show_buyers_button = QPushButton('Добавить покупателя', self.main_area)
-		self.show_buyers_button.setGeometry(609, 47, 230, 25)
-		self.show_buyers_button.clicked.connect(self.show_addbuyer_dialog)
+		self.add_buyers_button = QPushButton('Добавить покупателя', self.main_area)
+		self.add_buyers_button.setGeometry(609, 47, 230, 25)
+		self.add_buyers_button.clicked.connect(self.show_addbuyer_dialog)
 
 	def show_history(self):
+		"""
+			Загрзка записей покупок в окно главной таблицы.
+		"""
 		history = self.db.select_all()
 		self.show_table_data(history,
 							 ['Название товара', 'Цена', 'Артикул', 'Имя покупателя', 'Фамилия покупателя', 'Скидка', 'Телефон','Клубный статус',
 							  'Название магазина', 'Адрес магазина', 'Дата покупки'])
 
 	def show_bought_products(self):
+		"""
+			Загрзка записей о покупках в окно главной таблицы.
+		"""
 		bought_products = self.db.get_by_name(self.bought_products_name_lineedit.text(),
 											  self.bought_products_surname_lineedit.text())
 		self.show_table_data(bought_products,
@@ -119,20 +132,29 @@ class MainWindow(QMainWindow):
 							  'Дата покупки'])
 
 	def show_product_buyers(self):
+		"""
+			Загрзка записей о покупателях для конкретного товара в окно главной таблицы.
+		"""
 		bought_products = self.db.get_by_article(self.product_buyers_article_lineedit.text())
 		self.show_table_data(bought_products,
 							 ['Название товара', 'Цена', 'Фамилия покупателя', 'Имя покупателя', 'Скидка', 'Телефон','Клубный статус',
 							  'Название магазина', 'Адрес магазина', 'Дата покупки'])
 
 	def show_buyers(self):
+		"""
+			Загрзка записей о всех покупателях и их клубных картах в окно главной таблицы.
+		"""
 		bought_products = self.db.get_buyers()
 		self.show_table_data(bought_products,
 							 ['Имя покупателя', 'Фамилия покупателя', 'Скидка', 'Телефон','Клубный статус'])
 
 	def show_table_data(self, data, lables=['']):
+		"""
+			Преобразование ответа базы данных в строки таблицы и их вывод на экран.
+		"""
 		self.clear_main_table()
 		if (len(data)) == 0:
-			self.displayError()
+			self.display_error()
 			print('No data found to fill the table.')
 			return
 		self.main_table.verticalHeader().setVisible(True)
@@ -148,7 +170,10 @@ class MainWindow(QMainWindow):
 		self.main_table.setHorizontalHeaderLabels(lables)
 		self.main_table.resizeColumnsToContents()
 
-	def displayError(self):
+	def display_error(self):
+		"""
+			Вывод сообщения об отсутсвии записей в ответном сообщении.
+		"""
 		self.main_table.setColumnCount(1)
 		self.main_table.insertRow(0)
 		self.main_table.setItem(0, 0, QTableWidgetItem('Информация не найдена'))
@@ -157,6 +182,9 @@ class MainWindow(QMainWindow):
 		self.main_table.horizontalHeader().setVisible(False)
 
 	def clear_main_table(self):
+		"""
+			Очистка главной таблицы.
+		"""
 		self.main_table.clearContents()
 		while self.main_table.rowCount() > 0:
 			self.main_table.removeRow(0)
@@ -164,6 +192,9 @@ class MainWindow(QMainWindow):
 			self.main_table.removeColumn(0)
 
 	def show_addbuyer_dialog(self):
+		"""
+			Вывод диалогового окна добавления покупателя.
+		"""
 		self.dialog = QDialog()
 		self.dialog.setAttribute(QtCore.Qt.WA_DeleteOnClose)
 		self.dialog.setWindowTitle("Добавление покупателя")
@@ -174,7 +205,7 @@ class MainWindow(QMainWindow):
 		paramNames = ['Имя покупателя', 'Фамилия покупателя', 'Скидка', 'Телефон', 'Клубный статус']
 
 		for i in range(0, len(paramNames)):
-			self.addDialogParam(self.dialog, i, paramNames[i])
+			self.add_dialog_param(self.dialog, i, paramNames[i])
 
 		self.dialog.add_buyer_button = QPushButton('Добавить', self.dialog)
 		self.dialog.add_buyer_button.setGeometry(150, 265, 100, 25)
@@ -184,10 +215,16 @@ class MainWindow(QMainWindow):
 		self.dialog.exec()
 
 	def add_buyer(self):
+		"""
+			Добавление покупателя
+		"""
 		params = self.dialog.paramEditBoxes
 		self.db.insert_into_buyers(params[0].text(), params[1].text(), params[2].text(), params[3].text(), params[4].text())
 
-	def addDialogParam(self, dialog, i, paramName):
+	def add_dialog_param(self, dialog, i, paramName):
+		"""
+			Загрузка полей в диалоговое окно добавления покупателя.
+		"""
 		text = QLabel(paramName, dialog)
 		text.setGeometry(10, 10+i*40, 165, 20)
 		text.setAlignment(Qt.AlignCenter)
@@ -197,8 +234,10 @@ class MainWindow(QMainWindow):
 		dialog.paramNameLabels.append(text)
 		self.dialog.close()
 
-
 def init():
+	"""
+		Запуск окна программы.
+	"""
 	app = QApplication(sys.argv)
 
 	window = MainWindow()
