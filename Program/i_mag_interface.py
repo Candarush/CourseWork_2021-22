e69406040bd8b481a3871822659562ea3503a29e
Akimov Vladislav <candarush@gmail.com>
2021-12-21 19:08:15 +0300

    4

--- Program/i_mag_interface.py
+++ Program/i_mag_interface.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import QSize, Qt, pyqtSlot
 from PyQt5.QtGui import QFont, QTextLine
 from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QLineEdit, QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, \
 	QWidget
+import pandas as pd
 import sys
 
 sys.path.insert(0, './Program')
@@ -20,29 +21,31 @@ class MainWindow(QMainWindow):
 		super().__init__()
 		self.db = MyDb(sqlite3.connect(sys.path[1]+'/db.sqlite'))
 
-		self.setWindowTitle("IMag")
-		self.setFixedSize(QSize(1000, 600))
+		self.setWindowTitle("I-Mag")
+		self.setFixedSize(QSize(1200, 600))
 
 		self.login_area = QWidget(self)
 		self.setCentralWidget(self.login_area)
 
 		self.login_text = QLabel("Войти", self.login_area)
-		self.login_text.setGeometry(440, 224, 120, 16)
+		self.login_text.setGeometry(440, 224, 320, 16)
 		self.login_text.setAlignment(Qt.AlignCenter)
 
 		self.login_linedit = QLineEdit("", self.login_area)
-		self.login_linedit.setGeometry(440, 240, 120, 30)
+		self.login_linedit.setPlaceholderText('Введите логин')
+		self.login_linedit.setGeometry(440, 240, 320, 30)
 
 		self.password_text = QLabel("Password", self.login_area)
-		self.password_text.setGeometry(440, 274, 120, 16)
+		self.password_text.setGeometry(440, 274, 320, 16)
 		self.password_text.setAlignment(Qt.AlignCenter)
 
 		self.password_lineedit = QLineEdit("", self.login_area)
-		self.password_lineedit.setGeometry(440, 290, 120, 30)
+		self.password_lineedit.setPlaceholderText('Введите пароль')
+		self.password_lineedit.setGeometry(440, 290, 320, 30)
 		self.password_lineedit.setEchoMode(QLineEdit.Password)
 
-		self.authorize_button = QPushButton("Log in", self.login_area)
-		self.authorize_button.setGeometry(460, 330, 80, 40)
+		self.authorize_button = QPushButton("Авторизоваться", self.login_area)
+		self.authorize_button.setGeometry(460, 330, 280, 40)
 		self.authorize_button.clicked.connect(self.login)
 
 		self.show()
@@ -67,13 +70,14 @@ class MainWindow(QMainWindow):
 			Загрузка элементов главной таблицы.
 		"""
 		self.login_area.setParent(None)
-		print('Initialized.')
+		print('Инициализировано.')
 
 		self.main_area = QWidget(self)
 		self.setCentralWidget(self.main_area)
 
 		self.main_table = QTableWidget(self.main_area)
-		self.main_table.setGeometry(20, 80, 960, 500)
+		self.main_table.setSortingEnabled(True)
+		self.main_table.setGeometry(20, 80, 1160, 500)
 		self.main_table.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
 		self.main_table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn);
 
@@ -86,12 +90,12 @@ class MainWindow(QMainWindow):
 		self.bought_products_button.clicked.connect(self.show_bought_products)
 
 		self.bought_products_name_lineedit = QLineEdit(self.main_area)
-		self.bought_products_name_lineedit.setPlaceholderText('Имя покупателя')
+		self.bought_products_name_lineedit.setPlaceholderText('Имя')
 		self.bought_products_name_lineedit.setGeometry(131, 47, 113, 25)
 		self.bought_products_name_lineedit.setAlignment(Qt.AlignCenter)
 
 		self.bought_products_surname_lineedit = QLineEdit(self.main_area)
-		self.bought_products_surname_lineedit.setPlaceholderText('Фамилия покупателя')
+		self.bought_products_surname_lineedit.setPlaceholderText('Фамилия')
 		self.bought_products_surname_lineedit.setGeometry(246, 47, 113, 25)
 		self.bought_products_surname_lineedit.setAlignment(Qt.AlignCenter)
 
@@ -111,6 +115,47 @@ class MainWindow(QMainWindow):
 		self.add_buyers_button = QPushButton('Добавить покупателя', self.main_area)
 		self.add_buyers_button.setGeometry(609, 47, 230, 25)
 		self.add_buyers_button.clicked.connect(self.show_addbuyer_dialog)
+
+		
+		self.save_button = QPushButton('Сохранить в Excel', self.main_area)
+		self.save_button.setGeometry(847, 20, 230, 25)
+		self.save_button.clicked.connect(self.save_to_excel)
+		
+		self.filename_lineedit = QLineEdit(self.main_area)
+		self.filename_lineedit.setGeometry(847, 47, 230, 25)
+		self.filename_lineedit.setPlaceholderText('Название файла')
+		self.filename_lineedit.setAlignment(Qt.AlignCenter)
+
+
+	def save_to_excel(self):
+		"""
+			Сохранение данных таблицы в эксель файл.
+		"""
+		table = self.main_table
+		number_of_rows = table.rowCount()
+		number_of_columns = table.columnCount()
+
+		labels_list = []
+
+		for i in range(number_of_columns):
+			header = table.horizontalHeaderItem(i)
+			if header is not None:
+				labels_list.append(header.text())
+		if (len(labels_list) == 0):
+			self.display_error('Невозможно сохранить пустую таблицу!')
+			return
+
+		tmp_df = pd.DataFrame( 
+					columns=labels_list, # Fill columnets
+					index=range(number_of_rows) # Fill rows
+					) 
+
+		for i in range(number_of_rows):
+			for j in range(number_of_columns):
+				tmp_df.iloc[i, j] = table.item(i, j).data(0)
+
+		tmp_df.to_excel(excel_writer=self.filename_lineedit.text()+'.xlsx', index=False)
+		print('Сохранено')
 
 	def show_history(self):
 		"""
@@ -154,7 +199,7 @@ class MainWindow(QMainWindow):
 		"""
 		self.clear_main_table()
 		if (len(data)) == 0:
-			self.display_error()
+			self.display_error('Информация не найдена')
 			print('No data found to fill the table.')
 			return
 		self.main_table.verticalHeader().setVisible(True)
@@ -170,13 +215,13 @@ class MainWindow(QMainWindow):
 		self.main_table.setHorizontalHeaderLabels(lables)
 		self.main_table.resizeColumnsToContents()
 
-	def display_error(self):
+	def display_error(self, text):
 		"""
 			Вывод сообщения об отсутсвии записей в ответном сообщении.
 		"""
 		self.main_table.setColumnCount(1)
 		self.main_table.insertRow(0)
-		self.main_table.setItem(0, 0, QTableWidgetItem('Информация не найдена'))
+		self.main_table.setItem(0, 0, QTableWidgetItem(text))
 		self.main_table.resizeColumnsToContents()
 		self.main_table.verticalHeader().setVisible(False)
 		self.main_table.horizontalHeader().setVisible(False)
@@ -220,25 +265,42 @@ class MainWindow(QMainWindow):
 		"""
 		params = self.dialog.paramEditBoxes
 		self.db.insert_into_buyers(params[0].text(), params[1].text(), params[2].text(), params[3].text(), params[4].text())
+		self.dialog.close()
+		self.show_buyers()
 
 	def add_dialog_param(self, dialog, i, paramName):
 		"""
 			Загрузка полей в диалоговое окно добавления покупателя.
 		"""
 		text = QLabel(paramName, dialog)
-		text.setGeometry(10, 10+i*40, 165, 20)
-		text.setAlignment(Qt.AlignCenter)
+		text.setGeometry(10, 10+i*40, 145, 20)
+		text.setAlignment(Qt.AlignRight)
 		lineedit = QLineEdit("", dialog)
 		lineedit.setGeometry(175, 10+i*40, 165, 20)
 		dialog.paramEditBoxes.append(lineedit)
 		dialog.paramNameLabels.append(text)
-		self.dialog.close()
 
 def init():
 	"""
 		Запуск окна программы.
 	"""
 	app = QApplication(sys.argv)
+
+	#343d46
+	#4f5b66
+	#65737e
+	#a7adba
+	#c0c5ce
+
+	app.setStyleSheet("""
+		QLabel { color: #343d46; font-family: Verdana, sans-serif; font-weight: bold;}
+		QLineEdit { border: 1px solid #343d46; background-color: #ffffff; border-radius: 10px; color: #343d46; font-family: Verdana, sans-serif; font-weight: bold;}
+		QPushButton { border: 1px solid #343d46;  background-color: #65737e; border-radius: 10px; color: #ffffff; font-family: Verdana, sans-serif; font-weight: bold;}
+		QTableView { background-color: #ffffff; color: #000000; font-family: Verdana, sans-serif; }
+		QMainWindow { background-color: #c0c5ce}
+		QHeaderView::section { border: 1px solid #343d46;  background-color: #65737e; color: #ffffff; font-family: Verdana, sans-serif; font-weight: bold;}
+		QScrollBar { margin: -2px -2px -2px -2px; border: 2px solid #343d46;  background-color: #65737e; border-radius: 5px; color: #65737e; }
+	""")
 
 	window = MainWindow()
 	window.login_linedit.setText('admin')
